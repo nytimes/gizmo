fcacda61790bdef9b545e6d4114997638350b050
JP Robinson <john.paul.robinson@gmail.com>
2016-06-10 15:25:12 +0000

    filling in docs for pubsub/gcp

--- pubsub/gcp.go
+++ pubsub/gcp.go
@@ -9,6 +9,8 @@ import (
 	"google.golang.org/cloud/pubsub"
 )
 
+// GCPSubscriber is a Google Cloud Platform PubSub client that allows a user to
+// consume messages via the pubsub.Subscriber interface.
 type GCPSubscriber struct {
 	sub  *pubsub.Subscription
 	ctx  context.Context
@@ -18,6 +20,8 @@ type GCPSubscriber struct {
 	err  error
 }
 
+// NewGCPSubscriber will instantiate a new Subscriber that wraps
+// a pubsub.Iterator.
 func NewGCPSubscriber(ctx context.Context, cfg config.PubSub) (Subscriber, error) {
 	client, err := pubsub.NewClient(ctx, cfg.ProjectID)
 	if err != nil {
@@ -35,6 +39,7 @@ var (
 	defaultGCPMaxExtension = pubsub.MaxExtension(60 * time.Second)
 )
 
+// Start will start pulling from pubsub via a pubsub.Iterator.
 func (s *GCPSubscriber) Start() <-chan SubscriberMessage {
 	output := make(chan SubscriberMessage)
 	go func(s *GCPSubscriber, output chan SubscriberMessage) {
@@ -77,50 +82,57 @@ func (s *GCPSubscriber) Start() <-chan SubscriberMessage {
 
 }
 
+// Err will contain any error the Subscriber has encountered while processing.
 func (s *GCPSubscriber) Err() error {
 	return s.err
 }
 
-// Stop will block until the consumer has stopped consuming
-// messages.
+// Stop will block until the consumer has stopped consuming messages.
 func (s *GCPSubscriber) Stop() error {
 	exit := make(chan error)
 	s.stop <- exit
 	return <-exit
 }
 
+// GCPSubMessage pubsub implementation of pubsub.SubscriberMessage.
 type GCPSubMessage struct {
 	msg *pubsub.Message
 	ctx context.Context
 	sub string
 }
 
+// Message will return the data of the pubsub Message.
 func (m *GCPSubMessage) Message() []byte {
 	return m.msg.Data
 }
 
+// ExtendDoneDeadline will call the deprecated ModifyAckDeadline for a pubsub
+// Message. This likely should not be called.
 func (m *GCPSubMessage) ExtendDoneDeadline(dur time.Duration) error {
 	return pubsub.ModifyAckDeadline(m.ctx, m.sub, m.msg.ID, dur)
 }
 
+// Done will acknowledge the pubsub Message.
 func (m *GCPSubMessage) Done() error {
 	m.msg.Done(true)
 	return nil
 }
 
+// GCPPublisher is a Google Cloud Platform PubSub client that allows a user to
+// consume messages via the pubsub.Publisher interface.
 type GCPPublisher struct {
 	topic string
-	ctx   context.Context
 }
 
-func NewGCPPublisher(ctx context.Context, topic string) (Publisher, error) {
-	return &GCPPublisher{
+// NewGCPPublisher will instantiate a new GCPPublisher.
+func NewGCPPublisher(topic string) (Publisher, error) {
+	return GCPPublisher{
 		topic: topic,
-		ctx:   ctx,
 	}, nil
 }
 
-func (p *GCPPublisher) Publish(ctx context.Context, key string, msg proto.Message) error {
+// Publish will marshal the proto message and publish it to GCP pubsub.
+func (p GCPPublisher) Publish(ctx context.Context, key string, msg proto.Message) error {
 	mb, err := proto.Marshal(msg)
 	if err != nil {
 		return err
@@ -128,7 +140,8 @@ func (p *GCPPublisher) Publish(ctx context.Context, key string, msg proto.Messag
 	return p.PublishRaw(ctx, key, mb)
 }
 
-func (p *GCPPublisher) PublishRaw(ctx context.Context, key string, m []byte) error {
+// PublishRaw will publish the message to GCP pubsub.
+func (p GCPPublisher) PublishRaw(ctx context.Context, key string, m []byte) error {
 	_, err := pubsub.Publish(ctx, p.topic, &pubsub.Message{
 		Data:       m,
 		Attributes: map[string]string{"key": key},

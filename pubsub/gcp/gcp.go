ab7c3eacd3879c712254e0551e3757653410995d
JP Robinson <john.paul.robinson@gmail.com>
2016-08-02 17:38:07 -0400

    adding safety nets to pubsub

--- pubsub/gcp/gcp.go
+++ pubsub/gcp/gcp.go
@@ -1,6 +1,7 @@
 package gcp
 
 import (
+	"log"
 	"sync"
 	"time"
 
@@ -64,10 +65,18 @@ func (s *subscriber) Start() <-chan pubsub.SubscriberMessage {
 		for {
 			select {
 			case exit := <-s.stop:
-				iter.Stop()
+				log.Print("stopping iterator")
+				if iter != nil {
+					iter.Stop()
+				}
 				exit <- nil
 				return
 			default:
+				// something's wrong and we're on the way to stopping
+				if iter == nil {
+					continue
+				}
+
 				msg, err = iter.Next()
 				if err != nil {
 					s.err = err

24fde4f24bc80a0e57b5aaab29ebf666070d564c
JP Robinson <john.paul.robinson@gmail.com>
2017-06-04 22:11:20 -0400

    taking a whack at cloud endpoints

---
+++ examples/servers/kit/api/ce_create.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+gcloud service-management deploy ./service.pb ./ce_config.yaml
--- examples/servers/kit/api/gen-proto.sh
+++ examples/servers/kit/api/gen-proto.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
-openapi2proto -spec service.yaml > service.proto;
+openapi2proto -spec service.yaml -options > service.proto;
 
+# for our code
 protoc --go_out=plugins=grpc:. service.proto;
+
+# for CE setup
+protoc --include_imports --include_source_info service.proto --descriptor_set_out service.pb

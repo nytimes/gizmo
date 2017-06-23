935448a0dc05c7b4bed13690d8a17a3dadf999de
JP Robinson <john.paul.robinson@gmail.com>
2017-06-23 13:33:11 -0400

    adding additional http fields to the logger

--- server/kit/log.go
+++ server/kit/log.go
@@ -28,6 +28,9 @@ func LoggerWithFields(ctx context.Context) log.Logger {
 	// for HTTP requests
 	keys := map[interface{}]string{
 		http.ContextKeyRequestMethod:        "http-method",
+		http.ContextKeyRequestURI:           "http-uri",
+		http.ContextKeyRequestPath:          "http-path",
+		http.ContextKeyRequestHost:          "http-host",
 		http.ContextKeyRequestXRequestID:    "http-x-request-id",
 		http.ContextKeyRequestRemoteAddr:    "http-remote-addr",
 		http.ContextKeyRequestXForwardedFor: "http-x-forwarded-for",

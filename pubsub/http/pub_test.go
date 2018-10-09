08983748724a063c65cf9ff9336c92b1736f9b24
JP Robinson <john.paul.robinson@gmail.com>
2018-10-09 16:10:13 -0400

    adding tests

--- pubsub/http/pub_test.go
+++ pubsub/http/pub_test.go
@@ -10,6 +10,7 @@ import (
 	"testing"
 
 	"github.com/golang/protobuf/proto"
+	"github.com/google/go-cmp/cmp"
 )
 
 func TestPublishRaw(t *testing.T) {
@@ -257,4 +258,166 @@ func TestGCPPublish(t *testing.T) {
 		srv.Close()
 	}
 
+}
+
+func TestPublishMulti(t *testing.T) {
+	tests := []struct {
+		givenPayloads []proto.Message
+		givenErr      bool
+
+		wantPayloads []TestProto
+		wantErr      bool
+	}{
+		{
+			givenPayloads: []proto.Message{
+				&TestProto{"hi there!"},
+				&TestProto{"howdy!"},
+				&TestProto{"hello!"},
+			},
+
+			wantPayloads: []TestProto{{"hi there!"},
+				TestProto{"howdy!"},
+				TestProto{"hello!"},
+			},
+		},
+		{
+			givenPayloads: []proto.Message{
+				&TestProto{"hi there!"},
+				&TestProto{"howdy!"},
+				&TestProto{"hello!"},
+			},
+
+			givenErr: true,
+			wantErr:  true,
+		},
+	}
+
+	for _, test := range tests {
+		var resps []TestProto
+		srv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			body, err := ioutil.ReadAll(r.Body)
+			if err != nil {
+				t.Errorf("unable to read published request body: %s", err)
+			}
+
+			var got TestProto
+			err = proto.Unmarshal(body, &got)
+			if err != nil {
+				t.Errorf("unable to proto marshal published request body: %s", err)
+			}
+
+			resps = append(resps, got)
+
+			if test.givenErr {
+				w.WriteHeader(http.StatusServiceUnavailable)
+				io.WriteString(w, "doh!")
+				return
+			}
+			w.WriteHeader(http.StatusOK)
+		}))
+
+		pub := NewPublisher(srv.URL, nil)
+
+		gotErr := pub.PublishMulti(nil, nil, test.givenPayloads)
+
+		if test.wantErr && gotErr == nil {
+			t.Errorf("expected error response from publish but got none")
+		}
+		if !test.wantErr && gotErr != nil {
+			t.Errorf("expected no error response from publish but got one: %s", gotErr)
+		}
+		srv.Close()
+
+		if gotErr != nil {
+			return
+		}
+
+		if !cmp.Equal(test.wantPayloads, resps) {
+			t.Errorf("payloads did not match expectations:\n\n%s", cmp.Diff(test.wantPayloads, resps))
+		}
+	}
+}
+
+func TestGCPPublishMulti(t *testing.T) {
+	tests := []struct {
+		givenPayloads []proto.Message
+		givenErr      bool
+
+		wantPayloads []TestProto
+		wantErr      bool
+	}{
+		{
+			givenPayloads: []proto.Message{
+				&TestProto{"hi there!"},
+				&TestProto{"howdy!"},
+				&TestProto{"hello!"},
+			},
+
+			wantPayloads: []TestProto{{"hi there!"},
+				TestProto{"howdy!"},
+				TestProto{"hello!"},
+			},
+		},
+		{
+			givenPayloads: []proto.Message{
+				&TestProto{"hi there!"},
+				&TestProto{"howdy!"},
+				&TestProto{"hello!"},
+			},
+
+			givenErr: true,
+			wantErr:  true,
+		},
+	}
+
+	for _, test := range tests {
+		var resps []TestProto
+		srv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			body, err := ioutil.ReadAll(r.Body)
+			if err != nil {
+				t.Errorf("unable to read published request body: %s", err)
+			}
+
+			var msg gcpPayload
+			err = json.Unmarshal(body, &msg)
+			if err != nil {
+				t.Errorf("unable to json marshal published request body: %s", err)
+			}
+
+			var got TestProto
+			err = proto.Unmarshal(msg.Message.Data, &got)
+			if err != nil {
+				t.Errorf("unable to proto marshal published request body: %s", err)
+			}
+
+			resps = append(resps, got)
+
+			if test.givenErr {
+				w.WriteHeader(http.StatusServiceUnavailable)
+				io.WriteString(w, "doh!")
+				return
+			}
+			w.WriteHeader(http.StatusOK)
+		}))
+
+		pub := NewGCPStylePublisher(srv.URL, nil)
+
+		gotErr := pub.PublishMulti(nil, nil, test.givenPayloads)
+
+		if test.wantErr && gotErr == nil {
+			t.Errorf("expected error response from publish but got none")
+		}
+		if !test.wantErr && gotErr != nil {
+			t.Errorf("expected no error response from publish but got one: %s", gotErr)
+		}
+		srv.Close()
+
+		if gotErr != nil {
+			return
+		}
+
+		if !cmp.Equal(test.wantPayloads, resps) {
+			t.Errorf("payloads did not match expectations:\n\n%s", cmp.Diff(test.wantPayloads, resps))
+		}
+	}
 }

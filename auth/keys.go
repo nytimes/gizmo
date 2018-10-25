2bc02767a87f419715c2c1b59691235e177e0c8d
JP Robinson <john.paul.robinson@gmail.com>
2018-10-25 16:47:49 -0400

    starting on auth packages

---
+++ auth/keys.go
@@ -0,0 +1,147 @@
+package auth
+
+import (
+	"context"
+	"crypto/rsa"
+	"encoding/base64"
+	"encoding/json"
+	"errors"
+	"io/ioutil"
+	"math/big"
+	"net/http"
+	"regexp"
+	"strconv"
+	"sync"
+	"time"
+)
+
+// PublicKeySource is to be used by servers who need to acquire public key sets for
+// verifying inbound request's JWTs.
+type PublicKeySource interface {
+	Get(context.Context) (*PublicKeySet, error)
+}
+
+// NewReusePublicKeySource is a wrapper around PublicKeySources to only fetch a new key
+// set once the current key cache has expired.
+func NewReusePublicKeySource(set *PublicKeySet, src PublicKeySource) KeySource {
+	return reuseKeySource{ks: ks, src: src}
+}
+
+type reuseKeySource struct {
+	src KeySource
+
+	mu sync.Mutex
+	ks
+}
+
+func (r *reuseKeySource) Get(ctx context.Context) (*PublicKeySet, error) {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+	if r.ks.Expired() {
+		var err error
+		r.ks, err = r.src.Get(ctx)
+		return r.ks, err
+	}
+	return r.ks, nil
+}
+
+type PublicKeySet struct {
+	exp  time.Time
+	keys map[string]*rsa.PublicKey
+}
+
+func (ks PublicKeySet) Expired() bool {
+	return timeNow().Before(ks.exp)
+}
+
+func (ks PublicKeySet) GetKey(id string) (*rsa.PublicKey, error) {
+	key, ok := ks.keys[id]
+	if !ok {
+		return nil, errors.New("unkown key")
+	}
+	return key
+}
+
+// JSONKey represents a public or private key in JWK format.
+type JSONKey struct {
+	Kty string `json:"kty"`
+	Alg string `json:"alg"`
+	Use string `json:"use"`
+	Kid string `json:"Kid"`
+	N   string `json:"n"`
+	E   string `json:"e"`
+}
+
+// JSON represents a JWK Set object.
+type JSON struct {
+	Keys []*key `json:"keys"`
+}
+
+var reMaxAge = regexp.MustCompile("max-age=([0-9]*)")
+
+func NewFromURL(hc *http.Client, url string, defaultTTL time.Duration) (PublicKeySet, error) {
+	r, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		return nil, errors.Wrap(err, "unable to create request")
+	}
+
+	resp, err := hc.Do(r)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	ttl := defaultTTL
+	if ccHeader := resp.Header.Get("cache-control"); ccHeader != "" {
+		if match := reMaxAge.FindStringSubmatch(ccHeader); len(match) > 1 {
+			maxAgeSeconds, err := strconv.ParseInt(match[1], 10, 64)
+			if err != nil {
+				return nil, errors.Wrap(err, "unable to parse cache-control max age")
+			}
+			ttl = time.Second * time.Duration(maxAgeSeconds)
+		}
+	}
+
+	payload, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, errors.Wrap(err, "unable to read response")
+	}
+
+	return NewFromJSON(payload, ttl)
+}
+
+func NewFromJSON(payload []byte, ttl time.Duration) (PublicKeySet, error) {
+	var keys JSON
+	err = json.Unmarshal(payload, &keys)
+	if err != nil {
+		return err
+	}
+
+	ks := PublicKeySet{
+		exp:  timeNow().Add(ttl),
+		keys: map[string]*rsa.PublicKey{},
+	}
+
+	for _, key := range keys.Keys {
+		// we only plan on using RSA
+		if key.Use == "sig" && key.Kty == "RSA" {
+			n, err := base64.RawURLEncoding.DecodeString(key.N)
+			if err != nil {
+				return nil, errors.Wrap(err, "unable to decode key")
+			}
+			e, err := base64.RawURLEncoding.DecodeString(key.E)
+			if err != nil {
+				return nil, errors.Wrap(err, "unable to decode key")
+			}
+			ei := big.NewInt(0).SetBytes(e).Int64()
+			if err != nil {
+				return nil, errors.Wrap(err, "unable to setup key")
+			}
+			ks.keys[key.Kid] = &rsa.PublicKey{
+				N: big.NewInt(0).SetBytes(n),
+				E: int(ei),
+			}
+		}
+	}
+	return ks, nil
+}
---
+++ auth/verify.go
@@ -0,0 +1,101 @@
+package auth
+
+import (
+	"context"
+	"encoding/base64"
+	"encoding/json"
+	"errors"
+	"strings"
+	"time"
+
+	"golang.org/x/oauth2/jws"
+)
+
+type Verifier struct {
+	ks KeySource
+	df ClaimsDecoderFunc
+	vf VerifyFunc
+
+	skewAllowance int64
+}
+
+var defaultSkewAllowance = time.Minute * 5
+
+type ClaimSetter interface {
+	BaseClaims() *jws.ClaimSet
+}
+
+type ClaimsDecoderFunc func(context.Context, []byte) (ClaimSetter, error)
+
+type VerifyFunc func(context.Context, interface{}) bool
+
+func NewVerifier(ks KeySource, df ClaimsDecoderFunc) *Verifier {
+	return &Verifier{ks: ks, df: df, skewAllowance: defaultSkewAllowance}
+}
+
+func (c Verifier) Verify(ctx context.Context, token string) (bool, error) {
+	// decode token to get header
+	hdr, rawPayload, err := decodeToken(token)
+	if err != nil {
+		return false, err
+	}
+
+	// get keyset
+	keys, err := c.ks.GetKeys(ctx)
+	if err != nil {
+		return false, err
+	}
+
+	// get key from keyset
+	key, err := keys.GetKey(hdr.KeyID)
+	if err != nil {
+		return false, err
+	}
+
+	// verify token
+	err = jws.Verify(token, key)
+	if err != nil {
+		return false, err
+	}
+
+	// use claims decoder func
+	clmstr, err := c.df(ctx, rawPayload)
+	if err != nil {
+		return false, err
+	}
+
+	claims := clmstr.BaseClaims()
+
+	if nowUnix < (claims.Iat - c.skewAllowance) {
+		return false, errors.New("invalid issue time")
+	}
+
+	if nowUnix > (claims.Exp + c.skewAllowance) {
+		return false, errors.New("invalid expiration time")
+	}
+
+	return c.vf(ctx, clmstr), nil
+}
+
+func decodeToken(token string) (*jws.Header, []byte, error) {
+	s := strings.Split(token, ".")
+	if len(s) != 3 {
+		return nil, nil, errors.New("invalid token")
+	}
+
+	dh, err := base64.RawURLEncoding.DecodeString(s[0])
+	if err != nil {
+		return nil, nil, err
+	}
+	var h jws.Header
+	err = json.Unmarshal(dh, &h)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	dcs, err := base64.RawURLEncoding.DecodeString(s[1])
+	if err != nil {
+		return nil, nil, err
+	}
+	return &h, dcs, nil
+}

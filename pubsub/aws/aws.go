4529fd3b0731b5f31d0857882a059e984e465aee
Yee Hing Tong <wild-endeavor@users.noreply.github.com>
2018-11-05 15:18:18 -0800

    Update aws.go

--- pubsub/aws/aws.go
+++ pubsub/aws/aws.go
@@ -46,16 +46,14 @@ func NewPublisher(cfg SNSConfig) (pubsub.Publisher, error) {
 	var creds *credentials.Credentials
 	if cfg.AccessKey != "" {
 		creds = credentials.NewStaticCredentials(cfg.AccessKey, cfg.SecretKey, cfg.SessionToken)
-	} else {
-		creds = credentials.NewEnvCredentials()
-	}
-
-	if cfg.RoleARN != "" {
+	} else if cfg.RoleARN != "" {
 		var err error
 		creds, err = requestRoleCredentials(creds, cfg.RoleARN, cfg.MFASerialNumber)
 		if err != nil {
 			return p, err
 		}
+	} else {
+		creds = credentials.NewEnvCredentials()
 	}
 
 	sess, err := session.NewSession()

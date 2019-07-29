5c9516ab48ae3ffb5c11db95fdeacb32ed59ddc5
JP Robinson <john.paul.robinson@gmail.com>
2019-07-29 14:41:22 -0400

    better test error msgs

--- server/router_test.go
+++ server/router_test.go
@@ -35,13 +35,13 @@ func TestStdlibRoute(t *testing.T) {
 	srvr.Register(&benchmarkSimpleService{})
 
 	w := httptest.NewRecorder()
-	r, _ := http.NewRequest("GET", "/svc/v1/2", nil)
+	r := httptest.NewRequest("GET", "/svc/v1/2", nil)
 	r.RemoteAddr = "0.0.0.0:8080"
 
 	srvr.ServeHTTP(w, r)
 
 	if w.Code != http.StatusOK {
-		t.Errorf("SimpleHealthCheck expected 200 response code, got %d", w.Code)
+		t.Errorf("stdlib route expected 200 response code, got %d", w.Code)
 	}
 
 	wantBody := "ok"
